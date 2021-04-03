94dff61e2c77350e7d89395a29990ae77bbc8f86
Igor Mukhametdinov <mukhametdinovigor@gmail.com>
2021-04-03 23:11:13 +1100

    Add Pokemon model

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# your models here
+
+class Pokemon(models.Model):
+    title_ru = models.CharField(max_length=200)
+
+    def __str__(self):
+        return '{}'.format(self.title_ru)
