708771270a994612bbfd94f3cf4759982e7f1399
Igor Mukhametdinov <mukhametdinovigor@gmail.com>
2021-04-04 07:06:52 +1100

    Add __str__ into PokemonEntity model

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -20,3 +20,6 @@ class PokemonEntity(models.Model):
     Strength = models.IntegerField(blank=True, null=True)
     Defence = models.IntegerField(blank=True, null=True)
     Stamina = models.IntegerField(blank=True, null=True)
+
+    def __str__(self):
+        return '{}'.format(self.pokemon)
