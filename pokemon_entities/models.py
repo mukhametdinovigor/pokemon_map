3cda2dee97b14b16fa0ceda21b385ff68dd7e6c8
Igor Mukhametdinov <mukhametdinovigor@gmail.com>
2021-04-04 06:54:07 +1100

    Add PokemonEntity model

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -7,3 +7,16 @@ class Pokemon(models.Model):
 
     def __str__(self):
         return '{}'.format(self.title_ru)
+
+
+class PokemonEntity(models.Model):
+    pokemon = models.ForeignKey('Pokemon', on_delete=models.CASCADE, blank=True, null=True)
+    Latitude = models.FloatField()
+    Longitude = models.FloatField()
+    Appeared_at = models.DateTimeField(blank=True, null=True)
+    Disappeared_at = models.DateTimeField(blank=True, null=True)
+    Level = models.IntegerField(blank=True, null=True)
+    Health = models.IntegerField(blank=True, null=True)
+    Strength = models.IntegerField(blank=True, null=True)
+    Defence = models.IntegerField(blank=True, null=True)
+    Stamina = models.IntegerField(blank=True, null=True)
