3af09cefeaeb0ae30f0630fa17763896eb9ecf56
Igor Mukhametdinov <mukhametdinovigor@gmail.com>
2021-04-05 21:55:22 +1100

    Add reverse relation from Pokemon model to PokemonEntity model

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -17,6 +17,7 @@ class Pokemon(models.Model):
 
 class PokemonEntity(models.Model):
     pokemon = models.ForeignKey('Pokemon', on_delete=models.CASCADE,
+                                related_name='pokemon_entity',
                                 blank=True, null=True, verbose_name='Название покемона')
     Latitude = models.FloatField(verbose_name='Широта')
     Longitude = models.FloatField(verbose_name='Долгота')
